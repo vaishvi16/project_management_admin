ec0fa796c17f59b1a0c03c424a637b51683249eb
Vaishvi07 <91317372+Vaishvi07@users.noreply.github.com>
2025-11-14 17:17:24 +0530

    history screen view project api call

--- lib/screens/history_screen.dart
+++ lib/screens/history_screen.dart
@@ -11,16 +11,14 @@ class HistoryScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(title: const Text('History')),
       body: FutureBuilder(
         future: getProjects(),
         builder: (context, AsyncSnapshot snapshot) {
-          if(snapshot.hasError){
+          if (snapshot.hasError) {
             print("Error Api not found!");
-          }
-          else if(snapshot.hasData){
+          } else if (snapshot.hasData) {
             return ListView.builder(
               padding: const EdgeInsets.all(16),
               itemCount: snapshot.data.length,
@@ -31,13 +29,15 @@ class HistoryScreen extends StatelessWidget {
                   child: ListTile(
                     leading: const Icon(Icons.history, color: Colors.blue),
                     title: Text(item.title),
-                    subtitle: Text('${item.endDate.toLocal().toString().split(' ')[0]} by ${item.members.join(", ")}'),
+                    subtitle: Text(
+                      '${item.endDate.toLocal().toString().split(' ')[0]} by ${item.members.join(", ")}',
+                    ),
                   ),
                 );
               },
             );
           }
-          return CircularProgressIndicator();
+          return Center(child: CircularProgressIndicator());
         },
       ),
     );
@@ -50,7 +50,9 @@ class HistoryScreen extends StatelessWidget {
     var response = await http.get(url);
 
     if (response.statusCode == 200) {
-      print("Get project api working in history screeen!! ${response.body.toString()}");
+      print(
+        "Get project api working in history screeen!! ${response.body.toString()}",
+      );
       final jsonResponse = jsonDecode(response.body);
 
       final List<dynamic> projectsJson = jsonResponse['projects'] ?? [];
