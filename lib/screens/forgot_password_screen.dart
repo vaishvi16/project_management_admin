7bd83776f9e4c350462b16b2b8c328364a4178c7
rathoddhaval8335 <rathoddhaval8335@gmail.com>
2025-11-21 17:58:14 +0530

    validation complete forgot pass screen

--- lib/screens/forgot_password_screen.dart
+++ lib/screens/forgot_password_screen.dart
@@ -8,6 +8,13 @@ class ForgotPasswordScreen extends StatefulWidget {
 }
 
 class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController _emailcontoller = new TextEditingController();
+  TextEditingController _passcontoller = new TextEditingController();
+  TextEditingController _confirmpasscontoller = new TextEditingController();
+  bool _obscurePassword = true;
+  bool _obscureConfromPassword = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,42 +24,108 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
         foregroundColor: Colors.white,
       ),
 
-      body: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          children: [
-            TextFormField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: "Email",
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextFormField(
+                validator: (val) {
+                  if (val!.isEmpty) {
+                    return "Please Enter Your Email";
+                  }
+                  return null;
+                },
+                controller: _emailcontoller,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.email),
+                  labelText: "Email",
+                ),
               ),
-            ),
-            SizedBox(height: 15),
-            TextFormField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: "Password",
+              SizedBox(height: 15),
+              TextFormField(
+                validator: (val) {
+                  if (val!.isEmpty) {
+                    return "Please Enter Password";
+                  }
+                  if (val.length < 6) {
+                    return "Password Must Be at least 6 characters";
+                  }
+                  return null;
+                },
+                controller: _passcontoller,
+                  obscureText: _obscurePassword,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Password",
+                  prefixIcon: Icon(Icons.lock),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _obscurePassword
+                          ? Icons.visibility_off
+                          : Icons.visibility,
+                    ),
+                    onPressed: () =>
+                        setState(() => _obscurePassword = !_obscurePassword),
+                  ),
+                ),
               ),
-            ),
-            SizedBox(height: 15),
-            TextFormField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: "Confirm Password",
+              SizedBox(height: 15),
+              TextFormField(
+                validator: (val) {
+                  if (val!.isEmpty) {
+                    return "Please Enter Confirm Password";
+                  }
+                  return null;
+                },
+                controller: _confirmpasscontoller,
+                obscureText: _obscureConfromPassword,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Confirm Password",
+                  prefixIcon: Icon(Icons.lock),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _obscureConfromPassword
+                          ? Icons.visibility_off
+                          : Icons.visibility,
+                    ),
+                    onPressed: () =>
+                        setState(() => _obscureConfromPassword = !_obscureConfromPassword),
+                  ),
+                ),
               ),
-            ),
-            SizedBox(height: 15),
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blue,
-                foregroundColor: Colors.white,
+              SizedBox(height: 15),
+              ElevatedButton(
+                onPressed: _validationform,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  foregroundColor: Colors.white,
+                ),
+                child: Text("Update Password", style: TextStyle(fontSize: 15)),
               ),
-              child: Text("Update Password", style: TextStyle(fontSize: 15)),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  _validationform() {
+    if (_formKey.currentState!.validate()) {
+      String password=_passcontoller.text.toString();
+      String confirmpass=_confirmpasscontoller.text.toString();
+      if(password==confirmpass){
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(SnackBar(content: Text("Login Succesfully ")));
+      }else{
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(SnackBar(content: Text("password and confrompassword should be same ")));
+      }
+    }
   }
 }
